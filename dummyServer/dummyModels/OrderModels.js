ecdd13b96d517cac0b11d613c4e2e7da7e36268d
Fakorede Oluwakunle <fakoredeoluwakunle2626@gmail.com>
2018-09-02 13:42:28 +0100

    Adds dummy models for get all orders routes

---
+++ dummyServer/dummyModels/OrderModels.js
@@ -0,0 +1,22 @@
+const OrderModel = [
+  {
+    id: 1,
+    orderName: 'Jollof Rice',
+    amount: 600,
+    status: 'pending',
+  },
+  {
+    id: 2,
+    orderName: 'Fried Rice',
+    amount: 500,
+    status: 'completed',
+  },
+  {
+    id: 3,
+    orderName: 'Hamburger',
+    amount: 1500,
+    status: 'pending',
+  },
+];
+
+export default OrderModel;
