7596e86e35394af3b16032bcd248d6cdd5f8487b
Fakorede Oluwakunle <fakoredeoluwakunle2626@gmail.com>
2018-09-04 21:00:55 +0100

    [Chore #160263091] Test for orders endpoints

---
+++ test/OrderControllersTest.js
@@ -0,0 +1,103 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../server';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+// let authTokenLogin;
+// let authTokenSignup;
+
+
+/* global it, describe */
+
+// Test for POST requests
+describe('Orders', () => {
+  it('it should place an order', (done) => {
+    // HTTP POST -> PLACE AN ORDER
+    const newOrder = {
+      orderName: 'Jollof Rice',
+      amount: 600,
+      status: 'pending',
+    };
+    chai.request(app)
+      .post('/api/v1/orders')
+      .send(newOrder)
+      .end((err, res) => {
+        expect(res.body).to.have.property('message')
+          .eql('you have successfully Registered this Order');
+        expect(res.status).to.equal(201);
+        done();
+      });
+  });
+
+  it('it should fetch all orders', (done) => {
+    // HTTP GET -> FETCH ALL ORDERS;
+    chai.request(app)
+      .get('/api/v1/orders')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+
+  it('it should fetch a specific order', (done) => {
+    // HTTP POST -> GET A SPECIFIC ORDER;
+    const order = {
+      orderName: 'Jollof Rice',
+      amount: 600,
+      status: 'pending',
+    };
+    chai.request(app)
+      .get('/api/v1/orders/1')
+      .send(order)
+      .end((err, res) => {
+        expect(res.body).to.have.property('status')
+          .eql('success');
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+
+  it('it should not fetch a specific order that does not exit', (done) => {
+    // HTTP GET -> GET A SPECIFIC ORDER;
+    chai.request(app)
+      .get('/api/v1/orders/7')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        done();
+      });
+  });
+
+  it('it should update the status of an order', (done) => {
+    // HTTP POST -> LOGIN ADMIN
+    const order = {
+      status: 'completed',
+    };
+    chai.request(app)
+      .put('/api/v1/orders/1')
+      .send(order)
+      .end((err, res) => {
+        expect(res.body).to.have.property('message')
+          .eql('You have successfully updated your Order');
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+
+  it('it should not update an unknown order', (done) => {
+    // HTTP PUT -> UPDATE ORDER;
+    const order = {
+      status: 'completed',
+    };
+    chai.request(app)
+      .put('/api/v1/orders/5')
+      .send(order)
+      .end((err, res) => {
+        expect(res.body).to.have.property('message')
+          .eql('You are currently making a bad request');
+        expect(res.status).to.equal(400);
+        done();
+      });
+  });
+});
