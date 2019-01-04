9d67eab10430f64851742a1c903245885c653350
Oluwakunle Fakorede <oluwakunlefakorede@Oluwakunles-MacBook-Pro.local>
2019-01-04 22:35:22 +0100

    (chore) Setup react frontend project

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Fuck React!</h1>);
+  }
+}
+
+export default App;
